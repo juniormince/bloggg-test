033c8e15d75e6de014c04b708a816ad35e5802e5
bree <groundcontroltomajorbree@gmail.com>
2019-01-10 15:39:58 -0600

    yikes post test

--- src/pages/posts/2019-01-05-welcome/post.js
+++ src/pages/posts/2019-01-05-welcome/post.js
@@ -1,5 +1,5 @@
 export default {
-  title: `Congratulations on Your New Blog!`,
+  title: `HEY. LISTEN.`,
   tags: ['react', 'navi'],
   spoiler: "Learn how to add new posts and tweak the theme to your liking.",
   getContent: () => import('./document.mdx'),
---
+++ src/pages/posts/2019-01-10-yooo/post.js
@@ -0,0 +1,6 @@
+export default {
+    title: `SPOILER ALERT.`,
+    tags: ['privet', 'taggarino'],
+    spoiler: "does it work though.",
+    getContent: () => import('./document-dos.mdx'),
+  }
